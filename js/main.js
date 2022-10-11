cb06e5b93ee8918af896e3ceaa9deb50379d13ad
RohanNathHERE <rohan.nath.here@gmail.com>
2022-10-12 00:03:13 +0530

    Changes

--- js/main.js
+++ js/main.js
@@ -10,6 +10,14 @@ if (navToggle) {
         navMenu.classList.add('show-menu')
     })
 }
+
+/*==========MENU HIDDEN=======*/
+/*Validate if constant exists*/
+if (navClose) {
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu')
+    })
+}
 /*==========REMOVE MENU MOBILE==========*/
 
 /*==========ACCORDION SKILLS==========*/
